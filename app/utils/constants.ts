e734387a4e6b4256ccb610341110b35fe6c5edc3
kzoeps <kzoepa@gmail.com>
2023-11-11 14:37:10 -0500

    feat: add utility files

---
+++ app/utils/constants.ts
@@ -0,0 +1,10 @@
+export enum MealCodes {
+  "Breakfast" = '1',
+  "Lunch" = '2',
+  "Dinner" = '3',
+}
+
+export const HARRIS_URL =
+  "https://pepperbush.conncoll.edu/fpmobile.net/shortmenu.aspx?";
+
+export const ITEM_REGEX = /<div class='shortmenurecipes'>(?:<span[^>]*>)(.*)<\/div>/gm;
---
+++ app/utils/menu.ts
@@ -0,0 +1,63 @@
+import { ITEM_REGEX, HARRIS_URL, MealCodes } from "./constants";
+import { Meal } from "./types";
+
+export const getMealParams = (mealName: Meal, date: string) => {
+  const searchParams = {
+    locationNum: "01",
+    sName: "Connecticut College Dining Services",
+    locationName: "Harris",
+    naFlag: '1',
+    WeeksMenus: "This Week's Menus",
+    dtdate: date,
+    mealName,
+    lngcurselmeal: MealCodes[mealName],
+    savedallergens: "ýýýýýýýýýýALL",
+    savedwebcodes: "ýýýýýýALL",
+  };
+  return new URLSearchParams(searchParams);
+};
+
+const getMealMenu = async (params: URLSearchParams) => {
+    const fetchUrl = HARRIS_URL + params;
+    try {
+        const response = await fetch(fetchUrl);
+        const data = await response.text();
+        return data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+const getMealMenuItems = (html: string) => {
+  const matches = [...html.matchAll(ITEM_REGEX)];
+  const items = matches.map((match) => match[1]);
+  return items;
+};
+
+/**
+ * function to get the meal type for the current time
+ * @returns {Meal} - can be either Breakfast, Lunch or Dinner
+ */
+const getCurrentMeal = (): Meal => {
+  const currentTime = new Date();
+  let currentHour = currentTime.getHours();
+  const currentMinutesInHour = currentTime.getMinutes() / 60; 
+  currentHour += currentMinutesInHour;
+  console.log(currentHour);
+  if (currentHour < 11.5) {
+    return "Breakfast";
+  } else if (currentHour < 15) {
+    return "Lunch";
+  } else {
+    return "Dinner";
+  }
+}
+/**
+ *  function to get menu for a specific day 
+ * @param {Date} date 
+ */
+const getDailyMenu = (date: Date) => {
+  const dateParam = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  const menuParams = getMealParams(getCurrentMeal(), dateParam);
+  return getMealMenu(menuParams);
+}
